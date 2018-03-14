d4bcec56d2adf3c3826a73617cc060432a2986cf
Ryan <ryanflood444@gmail.com>
2018-03-14 22:13:20 +0000

    Added Oxygen, began camoflague

---
+++ Subtle Fish V2 - Radar Added/Assets/Scripts/Hide.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide : MonoBehaviour {
+
+	public Color color;
+	Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = gameObject.GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rend.material.color = color; 
+
+		if (Input.GetKeyDown (KeyCode.Mouse1))
+			color.a -= 0.1f;
+	}
+}
---
+++ Subtle Fish V2 - Radar Added/Assets/Scripts/PlayerOxygen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerOxygen : MonoBehaviour {
+
+	float oxygen = 100, maxOxygen = 100;
+	public float oxygenDecrease;
+	public Slider oxygenUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		oxygen -= oxygenDecrease;
+		oxygenUI.value = oxygen / maxOxygen; 
+
+		if (oxygen <= 0) {
+			SceneManager.LoadScene ("Test", LoadSceneMode.Single);
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Air") {
+			oxygen += maxOxygen - oxygen;
+			Destroy (coll.gameObject);
+			//coll.gameObject.GetComponent<AirBubbleDestroy> ().selfDestruct;
+		}
+	}
+}
