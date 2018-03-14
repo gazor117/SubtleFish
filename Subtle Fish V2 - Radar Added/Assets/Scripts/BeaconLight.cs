f87c0a065785f8693a7f704b67485efd2a14a96d
gazor117 <oreillygary7@gmail.com>
2018-03-14 22:36:10 +0000

    Basic Beacon Implemented

---
+++ Subtle Fish V2 - Radar Added/Assets/Scripts/BeaconLight.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeaconLight : MonoBehaviour {
+
+
+	Light beaconLight;
+	Color pink;
+	Color purple;
+	string[] colors = new string[4] {"yellow", "pink", "purple", "green"};
+	int randomIndex;
+	string randomColor;
+	int[] colorSequence;
+	float lightTimerAmount = 3f;
+	float lightCounter;
+	bool startTimer = false;
+	bool startSequenceTimer = false;
+	int sequenceLength = 4;
+	int currentNum = 0;
+	int currentColor = 1;
+	int color1, color2, color3, color4;
+	//bool sequenceComplete = false;
+
+
+
+
+
+
+	void Start () {
+		beaconLight = GetComponentInChildren<Light> ();
+		pink = new Color (255/255f, 192/255f, 203/225f);
+		purple = new Color (255/255f, 0 / 255f, 255/255f);
+
+		lightCounter = lightTimerAmount;
+
+	}
+
+
+	void Update () {
+		//Random ran = new Random ();
+		randomIndex = (Random.Range(0,4));
+		/*if (lightCounter < 0.1f) {
+			randomColor = colors [randomIndex];
+
+		}
+
+		if (randomColor == "blue") {
+			beaconLight.color = Color.blue;
+		}
+		if (randomColor == "pink") {
+			beaconLight.color = pink;
+		}
+		if (randomColor == "purple") {
+			beaconLight.color = purple;
+		}
+		if (randomColor == "green") {
+			beaconLight.color = Color.green;
+		}*/
+
+		currentNum ++;
+		if (currentNum == 1) {
+			color1 = randomIndex;
+		}if (currentNum == 2) {
+			color2 = randomIndex;
+		}if (currentNum == 3) {
+			color3 = randomIndex;
+		}if (currentNum == 4) {
+			color4 = randomIndex;
+			startSequenceTimer = true;
+		}
+
+		if (startSequenceTimer) {
+			if (currentColor == 1) {
+				if (colors [color1] == "yellow") {
+					beaconLight.color = Color.yellow;
+				}
+				if (colors [color1] =="pink") {
+					beaconLight.color = pink;
+				}
+				if (colors [color1] == "purple") {
+					beaconLight.color = purple;
+				}
+				if (colors [color1] == "green") {
+					beaconLight.color = Color.green;
+				}
+			}
+			if (currentColor == 2) {
+				if (colors [color2] == "yellow") {
+					beaconLight.color = Color.yellow;
+				}
+				if (colors [color2] == "pink") {
+					beaconLight.color = pink;
+				}
+				if (colors [color2] == "purple") {
+					beaconLight.color = purple;
+				}
+				if (colors [color2] == "green") {
+					beaconLight.color = Color.green;
+				}
+			}
+			if (currentColor == 3) {
+				if (colors [color3] == "yellow") {
+					beaconLight.color = Color.yellow;
+				}
+				if (colors [color3] == "pink") {
+					beaconLight.color = pink;
+				}
+				if (colors [color3] == "purple") {
+					beaconLight.color = purple;
+				}
+				if (colors [color3] == "green") {
+					beaconLight.color = Color.green;
+				}
+			}
+			if (currentColor == 4) {
+				if (colors [color4] == "yellow") {
+					beaconLight.color = Color.yellow;
+				}
+				if (colors [color4] == "pink") {
+					beaconLight.color = pink;
+				}
+				if (colors [color4] == "purple") {
+					beaconLight.color = purple;
+				}
+				if (colors [color4] == "green") {
+					beaconLight.color = Color.green;
+				}
+			}
+		}
+
+
+		Timer ();
+		print (currentColor);
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Player") {
+			print ("Player Detected");
+		}
+	}
+
+	void Timer()
+	{
+		if (startTimer) {
+
+			lightCounter -= Time.deltaTime;
+			if (lightCounter < 0) {
+				currentNum ++;
+				lightCounter = lightTimerAmount;
+				if (currentNum > sequenceLength -1) {
+					startTimer = false;
+				}
+			}
+		}
+
+		if (startSequenceTimer) {
+
+			lightCounter -= Time.deltaTime;
+			if (lightCounter < 0) {
+				beaconLight.color = Color.red;
+				currentColor ++;
+				lightCounter = lightTimerAmount;
+				if (currentColor > sequenceLength) {
+					currentColor = 1;
+				}
+			}
+		}
+	}
+}
