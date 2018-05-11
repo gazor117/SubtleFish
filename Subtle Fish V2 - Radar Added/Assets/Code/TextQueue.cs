9f33bf10a5ef121b0fe2c2504087567f23d1999c
CFerry96 <c16349346@mydit.ie>
2018-05-11 16:21:40 +0100

    Fungus triggers

---
+++ Subtle Fish V2 - Radar Added/Assets/Code/TextQueue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class TextQueue : MonoBehaviour {
+    public string Block;
+    public Flowchart flowstate;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        flowstate.ExecuteBlock(Block);
+        //Trigger Block//
+        Destroy(this.gameObject);
+    }
+
+}
