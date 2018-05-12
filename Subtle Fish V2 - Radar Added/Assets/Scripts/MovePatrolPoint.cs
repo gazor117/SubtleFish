4922baacbfff128c9ea6b0d1d53d1acbcf5738f1
gazor117 <oreillygary7@gmail.com>
2018-05-12 01:27:30 +0100

    Adjustments to tutorial scene

---
+++ Subtle Fish V2 - Radar Added/Assets/Scripts/MovePatrolPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePatrolPoint : MonoBehaviour {
+
+	public GameObject newPatrolPointPos;
+	public GameObject PatrolPoint;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Player") {
+			PatrolPoint.transform.position = newPatrolPointPos.transform.position;
+		}
+	}
+}
