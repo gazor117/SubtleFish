0e52cd40066660463f52a1a5fbbc0e3fb06d8e7c
Ryan <ryanflood444@gmail.com>
2018-05-14 20:20:14 +0100

    Cinematic Edit

---
+++ Subtle Fish V2 - Radar Added/Assets/Scripts/Cinematic_Scripts/EndOfCinematic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfCinematic : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void OnTriggerEnter2D () {
+		anim.SetBool ("Fade", true);
+		StartCoroutine (endScene ());
+	}
+
+	IEnumerator endScene () {
+		yield return new WaitForSeconds (2f);
+		SceneManager.LoadScene ("Menu", LoadSceneMode.Single);
+	}
+}
